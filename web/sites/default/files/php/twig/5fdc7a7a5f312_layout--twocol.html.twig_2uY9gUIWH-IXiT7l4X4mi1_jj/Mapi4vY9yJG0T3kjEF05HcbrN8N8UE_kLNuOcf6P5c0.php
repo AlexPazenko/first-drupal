db65e7ad0fb1a21701de63286c8103ac089b7ea8
Alex_Pazenko <apazenko.pazenko@ek-dt-020>
2020-12-21 18:15:29 +0200

    first commit

---
+++ web/sites/default/files/php/twig/5fdc7a7a5f312_layout--twocol.html.twig_2uY9gUIWH-IXiT7l4X4mi1_jj/Mapi4vY9yJG0T3kjEF05HcbrN8N8UE_kLNuOcf6P5c0.php
@@ -0,0 +1,157 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/stable/templates/layout/layout--twocol.html.twig */
+class __TwigTemplate_7780af85a47680ea2e75423da392056bd8d5e3fdb8ef1c46092d9ebc36635a5f extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 12, "if" => 17];
+        $filters = ["escape" => 18];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 12
+        $context["classes"] = [0 => "layout", 1 => "layout--twocol"];
+        // line 17
+        if (($context["content"] ?? null)) {
+            // line 18
+            echo "  <div";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+            echo ">
+    ";
+            // line 19
+            if ($this->getAttribute(($context["content"] ?? null), "top", [])) {
+                // line 20
+                echo "      <div ";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute(($context["region_attributes"] ?? null), "top", []), "addClass", [0 => "layout__region", 1 => "layout__region--top"], "method")), "html", null, true);
+                echo ">
+        ";
+                // line 21
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content"] ?? null), "top", [])), "html", null, true);
+                echo "
+      </div>
+    ";
+            }
+            // line 24
+            echo "
+    ";
+            // line 25
+            if ($this->getAttribute(($context["content"] ?? null), "first", [])) {
+                // line 26
+                echo "      <div ";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute(($context["region_attributes"] ?? null), "first", []), "addClass", [0 => "layout__region", 1 => "layout__region--first"], "method")), "html", null, true);
+                echo ">
+        ";
+                // line 27
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content"] ?? null), "first", [])), "html", null, true);
+                echo "
+      </div>
+    ";
+            }
+            // line 30
+            echo "
+    ";
+            // line 31
+            if ($this->getAttribute(($context["content"] ?? null), "second", [])) {
+                // line 32
+                echo "      <div ";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute(($context["region_attributes"] ?? null), "second", []), "addClass", [0 => "layout__region", 1 => "layout__region--second"], "method")), "html", null, true);
+                echo ">
+        ";
+                // line 33
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content"] ?? null), "second", [])), "html", null, true);
+                echo "
+      </div>
+    ";
+            }
+            // line 36
+            echo "
+    ";
+            // line 37
+            if ($this->getAttribute(($context["content"] ?? null), "bottom", [])) {
+                // line 38
+                echo "      <div ";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute(($context["region_attributes"] ?? null), "bottom", []), "addClass", [0 => "layout__region", 1 => "layout__region--bottom"], "method")), "html", null, true);
+                echo ">
+        ";
+                // line 39
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["content"] ?? null), "bottom", [])), "html", null, true);
+                echo "
+      </div>
+    ";
+            }
+            // line 42
+            echo "  </div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/stable/templates/layout/layout--twocol.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  125 => 42,  119 => 39,  114 => 38,  112 => 37,  109 => 36,  103 => 33,  98 => 32,  96 => 31,  93 => 30,  87 => 27,  82 => 26,  80 => 25,  77 => 24,  71 => 21,  66 => 20,  64 => 19,  59 => 18,  57 => 17,  55 => 12,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/stable/templates/layout/layout--twocol.html.twig", "/home/apazenko/sites/first-drupal/web/core/themes/stable/templates/layout/layout--twocol.html.twig");
+    }
+}
